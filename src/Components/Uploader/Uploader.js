8ec470d54f641ea1134caef86613f73d3046016e
Damon <damonbd@gmail.com>
2018-09-10 14:21:01 +0000

    uploader partial

--- src/Components/Uploader/Uploader.js
+++ src/Components/Uploader/Uploader.js
@@ -2,11 +2,42 @@ import React, { Component } from 'react';
 import './Uploader.css';
 
 class uploader extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            images: ""
+        }
+    }
+
+    handleImage = (e) => {
+        //validation
+
+        this.setState({ images: e.target.value });
+
+        var data = new FormData();
+        data.append("image", JSON.stringify(this.images));
+
+        fetch('http://localhost:8080/uploadImage/', {
+            method: 'POST',
+            body: data
+        }).then(
+           //is there .success or interro resp
+            () => {
+                //add to gallery below
+                alert("success post");
+            }
+        );
+        
+    }
+
     render() {
         return (
             <div className="backgroundColor"> 
                 <p>uploader component class</p>
-                <button className="btn btn-success">+ Upload</button>
+
+                <input onChange={this.handleImage} value={this.state.images} type="file" name="image" accept="image/*" />
+
             </div>
         );
     }
