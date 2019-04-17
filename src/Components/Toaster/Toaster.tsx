1377ae53039d85ccb62a62025b3084c2287f3faa
damonbd <damonbd@gmail.com>
2019-04-17 14:27:02 -0700

    updates toaster

--- src/Components/Toaster/Toaster.tsx
+++ src/Components/Toaster/Toaster.tsx
@@ -2,14 +2,21 @@ import React from 'react';
 
 import './Toaster.css'
 
-const Toaster = (props: any) => {
+interface IToasterProps {
+    body: string;
+    isSuccess: boolean;
+
+    hide: Function;
+}
+
+const Toaster = (props: IToasterProps) => {
     let backgroundColor = "toaster toaster-background-error";
     if (props.isSuccess) {
         backgroundColor = "toaster toaster-background-success";
     }
 
     return (
-        <div onClick={props.hide} className={backgroundColor}>
+        <div onClick={() => props.hide()} className={backgroundColor}>
             <div className="toaster-body">
                 {props.body}
             </div>
