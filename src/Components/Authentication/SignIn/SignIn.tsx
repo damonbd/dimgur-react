4baf01c62f81716187fcfdc6d8e30076d794fb8e
Damon <damonbd@gmail.com>
2020-01-12 17:03:06 -0800

    stubs out forgot pw

--- src/Components/Authentication/SignIn/SignIn.tsx
+++ src/Components/Authentication/SignIn/SignIn.tsx
@@ -15,6 +15,8 @@ interface ISignInProps {
 interface ISignInState {
     username: string;
     password: string;
+    email: string;
+    showPasswordResetForm: boolean;
 }
 
 class SignIn extends Component<ISignInProps, ISignInState> {
@@ -23,6 +25,8 @@ class SignIn extends Component<ISignInProps, ISignInState> {
         this.state = {
             username: "",
             password: "",
+            email: "",
+            showPasswordResetForm: false
         }
 
         this.handleInputChange = this.handleInputChange.bind(this);
@@ -68,17 +72,69 @@ class SignIn extends Component<ISignInProps, ISignInState> {
         }
     }
 
+    resetPasswordClick = () => {
+        // imaginary ajax call
+        let isValid = true;
+
+        if (isValid) {
+            this.props.toasterHandler(true, true, "Password Reset Successful.");
+            this.setState({
+                showPasswordResetForm: false,
+                email: "",
+                username: "",
+                password: ""
+            });
+        }
+        else {
+            this.props.toasterHandler(true, false, "Error: Something vague went wrong!");
+        }
+    }
+
+    showPasswordResetClick = () => {
+
+        console.log('cloicked')
+
+        this.setState({
+            showPasswordResetForm: !this.state.showPasswordResetForm,
+            email: "",
+            username: "",
+            password: ""
+        });
+    }
+
     render() {
+        let toRender = null;
+
+        if (!this.state.showPasswordResetForm) {
+            toRender = (
+                <div>
+                    <div className="auth-form">
+                        <input type="text" value={this.state.username} onChange={this.handleInputChange} id="username" name="username" placeholder="Username" className="auth-input" autoFocus />
+                        <input type="password" value={this.state.password} onChange={this.handleInputChange} id="password" name="password" placeholder="Password" className="auth-input" />
+                    </div>
+                    <div className="auth-form-button-group">
+                        <a onClick={this.showPasswordResetClick} className="auth-forgot"> Forgot Password? </a>
+                        <button onClick={this.signInClick} className="btn btn-primary auth-submit"> Submit </button>
+                    </div>
+                </div>
+            )
+        }
+        else {
+            toRender = (
+                <div>
+                    <div className="auth-form">
+                        <input type="text" value={this.state.email} onChange={this.handleInputChange} id="email" name="email" placeholder="Email" className="auth-input" autoFocus />
+                    </div>
+                    <div className="auth-form-button-group">
+                        <a onClick={this.showPasswordResetClick} className="auth-forgot"> Back to Sign in? </a>
+                        <button onClick={this.resetPasswordClick} className="btn btn-primary auth-submit"> Reset Password </button>
+                    </div>
+                </div>
+            )
+        }
+
         return (
-            <div>
-                <div className="auth-form">
-                    <input type="text" value={this.state.username} onChange={this.handleInputChange} id="username" name="username" placeholder="Username" className="auth-input" autoFocus />
-                    <input type="password" value={this.state.password} onChange={this.handleInputChange} id="password" name="password" placeholder="Password" className="auth-input" />
-                </div>
-                <div className="auth-form-button-group">
-                    <button onClick={this.signInClick} className="btn btn-primary auth-submit"> Submit </button>
-                </div>
-            </div>
+            toRender
         );
     }
 }
